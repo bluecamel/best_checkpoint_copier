b373703a1bcaf399ca66a6581f0ac23ade99ee89
Perplumblewintz Jonz <bluecamel@dlubuntu.b.lu>
2018-12-29 11:12:20 +0000

    fix compare

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='best_checkpoint_copier',
-      version='0.2',
+      version='0.3',
       description='Tensorflow Exporter that copies the best checkpoints',
       url='http://github.com/bluecamel/best_checkpoint_copier',
       author='Branton Davis',
